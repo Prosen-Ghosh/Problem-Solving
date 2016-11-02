cf31f1e80f8e218eb6a31f27c767aaad86c14ddf
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-02 23:48:12 +0600

    2/11/16

--- HackerRank - Bigger is Greater.cpp
+++ HackerRank - Bigger is Greater.cpp
@@ -1,6 +1,14 @@
+
+Solution:
+
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
+    *
+    * Prosen Ghosh
+    * American International University - Bangladesh (AIUB)
+    *
+*/
 int main() {
     int n;
     cin >> n;
