7e104858c1dca4279f44b272628c41b060658f51
Prosen-Ghosh <prosenghosh25@gmail.com>
2016-11-02 23:45:02 +0600

    2/11/16

---
+++ HackerRank - Bigger is Greater.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string str;cin >> str;
+        if(next_permutation(str.begin(),str.end())) cout << str << endl;
+        else cout << "no answer" << endl;
+    }
+    return 0;
+}
