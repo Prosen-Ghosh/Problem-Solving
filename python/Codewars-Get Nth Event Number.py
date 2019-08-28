89be0db57e076b5c93559676abe2dffaf2ecca13
Prosen-Ghosh <prosenghosh25@gmail.com>
2019-08-29 03:02:42 +0600

    Codewars-Get Nth Event Number solution

---
+++ python/Codewars-Get Nth Event Number.py
@@ -0,0 +1,4 @@
+def nth_even(n):
+   return 2 * n - 2
+
+# TUTORIAL: https://www.khanacademy.org/math/algebra/x2f8bb11595b61c86:sequences/x2f8bb11595b61c86:constructing-arithmetic-sequences/v/finding-the-100th-term-in-a-sequence
