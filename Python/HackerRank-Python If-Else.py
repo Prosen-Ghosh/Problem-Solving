9e1ad163e89a678ef56f898c968bd937061e6efe
Prosen Ghosh <prosenghosh25@gmail.com>
2019-10-01 00:24:36 +0600

    Python If-Else Solution

---
+++ Python/HackerRank-Python If-Else.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 != 0:
+        print('Weird')
+    elif n % 2 == 0 and n >= 2 and n <= 5:
+        print('Not Weird')
+    elif n % 2 == 0 and n >= 6 and n <= 20:
+        print('Weird')
+    elif n % 2 == 0 and n >= 20:
+        print('Not Weird')
