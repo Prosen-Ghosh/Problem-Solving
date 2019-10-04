c33fc893a12e7e0c5189189f59c3edf33827585d
Prosen Ghosh <prosenghosh25@gmail.com>
2019-10-04 19:46:38 +0600

    HackerRank-Capitalize! Solution

---
+++ Python/HackerRank-Capitalize!.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    return " ".join([x.capitalize() for x in s.split(" ")])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
