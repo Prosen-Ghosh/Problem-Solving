82d537e0827106151fb598398b7d7d8542686313
Prosen Ghosh <prosenghosh25@gmail.com>
2022-01-24 19:02:01 +0600

    Solve: Quarter of the year

---
+++ JavaScript/codewars/Quarter of the year.js
@@ -0,0 +1,11 @@
+const quarterOf = (month) => {
+    if (month >= 1 && month <= 3) {
+        return 1;
+    } else if (month >= 4 && month <= 6) {
+        return 2;
+    } else if (month >= 7 && month <= 9) {
+        return 3;
+    } else if (month >= 10 && month <= 12) {
+        return 4;
+    }
+};
