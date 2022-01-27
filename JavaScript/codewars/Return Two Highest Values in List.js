5ef558faec58ea00a004de1a087b3ba77cc9bbd4
Prosen Ghosh <prosenghosh25@gmail.com>
2022-01-27 22:16:29 +0600

    solve: Return Two Highest Values in List code wars

--- JavaScript/codewars/Return Two Highest Values in List.js
+++ JavaScript/codewars/Return Two Highest Values in List.js
@@ -1,6 +1,10 @@
+// function twoHighest(arr) {
+//   const newArr = [...new Set(arr.sort((a, b) => a - b))];
+//   if (!newArr || newArr.length === 0) return [];
+//   if (newArr.length === 1) return arr;
+//   return [newArr[newArr.length - 1], newArr[newArr.length - 2]];
+// }
+
 function twoHighest(arr) {
-  const newArr = [...new Set(arr.sort((a, b) => a - b))];
-  if (!newArr || newArr.length === 0) return [];
-  if (newArr.length === 1) return arr;
-  return [newArr[newArr.length - 1], newArr[newArr.length - 2]];
+  return [...new Set(arr)].sort((a, b) => b - a).slice(0, 2);
 }
