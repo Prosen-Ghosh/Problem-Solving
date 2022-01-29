e3ae604838474b44c3015c3c3412a9b312979301
Prosen Ghosh <prosenghosh25@gmail.com>
2022-01-29 21:41:51 +0600

    solve: Fundamentals: Return code wars

---
+++ JavaScript/codewars/Fundamentals: Return.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    return a + b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function mod(a,b){
+    return a % b;
+}
+   
+function exponent(a,b){
+    return a ** b;
+}
+    
+function subt(a,b){
+    return a - b;
+}
